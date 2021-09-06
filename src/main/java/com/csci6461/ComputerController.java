0ac0ec024103634906a5da3de8c52bc9bd8dcfcd
Ellis Thompson <ellisthompson.1999@outlook.com>
2021-09-06 13:50:31 -0400

    Added GPRs 0-3

--- src/main/java/com/csci6461/ComputerController.java
+++ src/main/java/com/csci6461/ComputerController.java
@@ -35,11 +35,28 @@ public class ComputerController {
     private CheckBox mbr1,mbr2,mbr4,mbr8,mbr16,mbr32,mbr64,mbr128,mbr256,mbr512,mbr1024,mbr2048,mbr4096,
             mbr8192,mbr16384,mbr32768;
 
+    @FXML
+    private CheckBox gpr10,gpr20,gpr40,gpr80,gpr160,gpr320,gpr640,gpr1280,gpr2560,gpr5120,gpr10240,gpr20480,gpr40960,
+            gpr81920,gpr163840,gpr327680;
+    @FXML
+    private CheckBox gpr11,gpr21,gpr41,gpr81,gpr161,gpr321,gpr641,gpr1281,gpr2561,gpr5121,gpr10241,gpr20481,gpr40961,
+            gpr81921,gpr163841,gpr327681;
+    @FXML
+    private CheckBox gpr12,gpr22,gpr42,gpr82,gpr162,gpr322,gpr642,gpr1282,gpr2562,gpr5122,gpr10242,gpr20482,gpr40962,
+            gpr81922,gpr163842,gpr327682;
+    @FXML
+    private CheckBox gpr13,gpr23,gpr43,gpr83,gpr163,gpr323,gpr643,gpr1283,gpr2563,gpr5123,gpr10243,gpr20483,gpr40963,
+            gpr81923,gpr163843,gpr327683;
+
     ToggleButton[] bitController;
 
     CheckBox[] pcController;
     CheckBox[] marController;
     CheckBox[] mbrController;
+    CheckBox[] gpr0Controller;
+    CheckBox[] gpr1Controller;
+    CheckBox[] gpr2Controller;
+    CheckBox[] gpr3Controller;
 
     /**
      * Handles loading of bits for the program counter.
@@ -76,6 +93,58 @@ public class ComputerController {
         }
 
         translateBits(mbrController);
+    }
+
+    /**
+     * Handles loading of bits for the GPR0.
+     */
+    @FXML
+    protected void onGPR0LoadClick() {
+        if (gpr0Controller == null){
+            gpr0Controller = new CheckBox[]{gpr10,gpr20,gpr40,gpr80,gpr160,gpr320,gpr640,gpr1280,gpr2560,gpr5120,
+                    gpr10240,gpr20480,gpr40960,gpr81920,gpr163840,gpr327680};
+        }
+
+        translateBits(gpr0Controller);
+    }
+
+    /**
+     * Handles loading of bits for the GPR0.
+     */
+    @FXML
+    protected void onGPR1LoadClick() {
+        if (gpr1Controller == null){
+            gpr1Controller = new CheckBox[]{gpr11,gpr21,gpr41,gpr81,gpr161,gpr321,gpr641,gpr1281,gpr2561,
+                    gpr5121,gpr10241,gpr20481,gpr40961, gpr81921,gpr163841,gpr327681};
+        }
+
+        translateBits(gpr1Controller);
+    }
+
+    /**
+     * Handles loading of bits for the GPR0.
+     */
+    @FXML
+    protected void onGPR2LoadClick() {
+        if (gpr2Controller == null){
+            gpr2Controller = new CheckBox[]{gpr12,gpr22,gpr42,gpr82,gpr162,gpr322,gpr642,gpr1282,gpr2562,gpr5122,
+                    gpr10242,gpr20482,gpr40962, gpr81922,gpr163842,gpr327682};
+        }
+
+        translateBits(gpr2Controller);
+    }
+
+    /**
+     * Handles loading of bits for the GPR3.
+     */
+    @FXML
+    protected void onGPR3LoadClick() {
+        if (gpr3Controller == null){
+            gpr3Controller = new CheckBox[]{gpr13,gpr23,gpr43,gpr83,gpr163,gpr323,gpr643,gpr1283,gpr2563,
+                    gpr5123,gpr10243,gpr20483,gpr40963, gpr81923,gpr163843,gpr327683};
+        }
+
+        translateBits(gpr3Controller);
     }
 
 
