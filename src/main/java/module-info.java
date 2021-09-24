3eef280f8ca9141d80f53b4a9ed95fde45985bd6
Ellis Thompson <ellisthompson.1999@outlook.com>
2021-09-24 19:14:58 -0400

    Added to pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,11 @@
 module com.csci6461 {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
+
+    //requires com.dlsc.formsfx;
+    //requires org.kordamp.bootstrapfx.core;
+    //requires java.desktop;
 //    requires org.testng;
 
     opens com.csci6461 to javafx.fxml;
